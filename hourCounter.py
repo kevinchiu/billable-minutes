df9c8ae4850ac2a4d326f9687c15a69955bc8f2e
kevingc <kevin.gc@gmail.com>
2008-11-09 06:07:02 -0500

    added another test, brushed up some code

--- hourCounter.py
+++ hourCounter.py
@@ -16,13 +16,12 @@ def minutes(time):
 	ampm = match.groups()[2]
 
 	hours = int(h)
-	if int(h) == 12:
+	if hours == 12:
 		hours = 0
 
 	minutes = int(m)
 	if ampm == "pm":
 		minutes += 12*60
-
 	minutes += hours*60
 
 	return minutes
@@ -89,9 +88,12 @@ def run_tests():
 	print_failed("multirange", str(test(780 == multirange("5:00am - 6:01am, 7:00am - 6:59pm"))))
 
 	# 12:00am - 1:10am, 10:37am - 12:54pm, 1:33pm - 7:00pm
-	# (13*60+10) - 12*60 + (12*60 + 54) - (10*60 + 37) + 19*60 - (13*60+33)
+	# (13*60+10) - 12*60 + (12*60 + 54) - (10*60 + 37) + 19*60 - (13*60+33) = 534
 	print_failed("multirange2", str(test(534 == multirange("12:00am - 1:10am, 10:37am - 12:54pm, 1:33pm - 7:00pm"))))
 
-	# 12:01am 
+	# 12:01am = minute 1
 	print_failed("ampm test", str(test(1 == minutes("12:01am"))))
+
+	# 12:00am = minute 0
+	print_failed("ampm test 2", str(test(0 == minutes("12:00am"))))
 
